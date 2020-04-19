4a9aef4360d1314e63981505ca4718cfb13a5390
niww <niww@ya.ru>
2020-04-19 22:22:28 +0300

    not add SharedPreferences

--- app/src/main/java/com/example/tbapp/MainActivity.kt
+++ app/src/main/java/com/example/tbapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.tbapp
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -7,23 +9,36 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.tbapp.data.DataRecycler
+import com.example.tbapp.ui.home.HomeFragment
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+//    val DATE = "date"
+//    val mSettings = getSharedPreferences(DATE, Context.MODE_PRIVATE)
+//    val editor = mSettings.edit()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
+
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+    }
+
+    override fun onPause() {
+        super.onPause()
     }
 }
--- app/src/main/java/com/example/tbapp/ui/training/TrainingFragment.kt
+++ app/src/main/java/com/example/tbapp/ui/training/TrainingFragment.kt
@@ -1,19 +1,17 @@
 package com.example.tbapp.ui.training
 
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatSpinner
-import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 import com.example.tbapp.R
-import java.util.zip.Inflater
+import java.util.*
 
 class TrainingFragment : Fragment() { //todo made fragment
+
 
     val TAG  = "Train"
     override fun onCreateView(
@@ -22,7 +20,33 @@ class TrainingFragment : Fragment() { //todo made fragment
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_training, container, false)
+        val root = inflater.inflate(R.layout.fragment_training, container, false)
+//        view.findViewById<AppCompatTextView>(R.id.tvCurrentDay).text
+        val btnStart = root.findViewById<AppCompatButton>(R.id.btn_start)
+        val btnStop = root.findViewById<AppCompatButton>(R.id.btn_stop)
+        val tvStart = root.findViewById<AppCompatTextView>(R.id.tv_start)
+        val tvStop = root.findViewById<AppCompatTextView>(R.id.tv_stop)
+        val tvResult = root.findViewById<AppCompatTextView>(R.id.tv_result)
+
+        val timeStore = mutableListOf<Long>(1)
+
+        btnStart.setOnClickListener {
+            tvStart.text = Date().toString()
+            timeStore.add(0, Date().time)
+        }
+
+        btnStop.setOnClickListener {
+            tvStop.text = Date().toString()
+            timeStore.add(1, Date().time)
+            if (timeStore.size == 1) {
+                timeStore.add(0, Date().time)
+            }
+            timeStore.add(2, timeStore[1] - timeStore[0])
+            tvResult.text = (timeStore[2]/1000).toString()
+//                tvResult.text = SimpleDateFormat("mm:ss",Locale.getDefault()).parse(timeStore[2].toString()).toString()
+        }
+
+        return root
     }
 
 
